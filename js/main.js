af9b387d20eb90496047cec68dacfb08def08e85
54mur41mx <c.albert15va@gmail.com>
2022-12-12 13:47:51 -0600

    Se soluciona primer version

--- js/main.js
+++ js/main.js
@@ -1,1 +1,26 @@
-//Aqui se escribirá el javascript
+//Aqui se escribirá el javascript.
+
+let nombre = "Abraham"
+
+nombre = nombre.toUpperCase();
+
+let letra="";
+
+let contaFinal=0;
+
+for (let index = 0; index < nombre.length; index++) {
+    console.log(nombre.charAt(index));
+    let conTemp=0;
+    for (let cont = 0; cont < nombre.length; cont++) {
+        if(nombre.charAt(index) == nombre.charAt(cont))
+            conTemp++
+    }
+
+    if (conTemp>contaFinal){
+        contaFinal=conTemp
+        letra = nombre.charAt(index);
+    }
+    
+}
+
+console.log(letra, contaFinal);
